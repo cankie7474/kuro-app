8b3092e6c91846eb9fbf2cb351fe38d13f5a2bef
cankie7474 <cankayacbon@gmail.com>
2026-05-10 01:33:41 +0200

    darken android navigation area

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,10 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import * as SystemUI from "expo-system-ui";
 import { AuthProvider } from "../context/AuthContext";
+import { colors } from "../styles/global";
+
+SystemUI.setBackgroundColorAsync(colors.background);
 
 export default function RootLayout() {
   return (
